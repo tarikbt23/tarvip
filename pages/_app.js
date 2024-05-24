77131e04d1bb73083cdcb1af1697c118b16a8923
tarikbt23 <213301050@ogr.selcuk.edu.tr>
2024-05-24 10:14:58 +0300

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,9 @@
 import Layout from '../components/Layout';
 import '../styles/globals.css';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <><Head>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap"
-        rel="stylesheet" />
-    </Head><Layout>
+    <><Layout>
         <Component {...pageProps} />
       </Layout></>
   );
